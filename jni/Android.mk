3e386432c9f157295ea5e4f221d445a8acec691a
wb7898 <wb7898@arcsoft.com.cn>
2023-01-18 17:24:01 +0800

    1

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,8 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libaddjni
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
-#LOCAL_SRC_FILES := $(call all-subdir-cpp-files) $(call all-subdir-c-files)
-LOCAL_SRC_FILES := add.cpp
+LOCAL_SRC_FILES := $(call all-cpp-files-under, ./)
 # All of the shared libraries we link against.
 LOCAL_LDLIBS := -pthread
 LOCAL_CFLAGS := -Wall -Werror -Wextra -Wno-unused-parameter -Wno-return-type -Wno-unused-variable -Wno-unused-function
